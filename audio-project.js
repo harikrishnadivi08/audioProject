d3b1936086d55378dc8fff1083059284d3a48abe
harikrishnadivi08 <harikrishna.divi08@gmail.com>
2020-12-19 14:05:24 -0500

    added play/pause functionality- all songs
    Added play/pause functionality for all songs

--- audio-project.js
+++ audio-project.js
@@ -11,6 +11,7 @@ button1.addEventListener ('click', function(){
     }
 })
 
+
 var button2 = document.getElementById("button2");
 var audio2 = document.getElementById("songBuddy");
 
@@ -21,5 +22,63 @@ button2.addEventListener ('click', function(){
     } else {
         audio2.pause();
         button2.innerHTML = '<i class="far fa-play-circle fa-5x"></i>';
+    }
+})
+
+
+var button3 = document.getElementById("button3");
+var audio3 = document.getElementById("songCute");
+
+button3.addEventListener ('click', function(){
+    if (audio3.paused) {
+        audio3.play();
+        button3.innerHTML = '<i class="far fa-pause-circle fa-5x"></i>';
+    } else {
+        audio3.pause();
+        button3.innerHTML = '<i class="far fa-play-circle fa-5x"></i>';
+    }
+})
+
+
+var button4 = document.getElementById("button4");
+var audio4 = document.getElementById("songMemories");
+
+button4.addEventListener ('click', function(){
+    if (audio4.paused) {
+        audio4.play();
+        button4.innerHTML = '<i class="far fa-pause-circle fa-5x"></i>';
+    } else {
+        audio4.pause();
+        button4.innerHTML = '<i class="far fa-play-circle fa-5x"></i>';
+    }
+})
+
+
+
+var button5 = document.getElementById("button5");
+var audio5 = document.getElementById("songGoingHigher");
+
+button5.addEventListener ('click', function(){
+    if (audio5.paused) {
+        audio5.play();
+        button5.innerHTML = '<i class="far fa-pause-circle fa-5x"></i>';
+    } else {
+        audio5.pause();
+        button5.innerHTML = '<i class="far fa-play-circle fa-5x"></i>';
+    }
+})
+
+
+
+var button6 = document.getElementById("button6");
+var audio6 = document.getElementById("songAcousticBreeze");
+
+button6.addEventListener ('click', function(){
+    if (audio6.paused) {
+        audio6.play();
+        button6.innerHTML = '<i class="far fa-pause-circle fa-5x"></i>';
+    } else {
+        audio6.pause();
+        button6.innerHTML = '<i class="far fa-play-circle fa-5x"></i>';
     }
 })
